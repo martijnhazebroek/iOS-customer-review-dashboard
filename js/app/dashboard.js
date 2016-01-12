05dd387e2a36a74ab0a61b2bd57071750bc21504
Martijn Hazebroek <git@martijnhazebroek.nl>
2016-01-12 20:38:37 +0100

    Added search and sort

--- js/app/dashboard.js
+++ js/app/dashboard.js
@@ -1,86 +1,89 @@
-define(['jquery', 'app/configurator', 'mustache', 'app/logger', 'app/datasource'], function($, configurator, templateEngine, logger, datasource) {
+define(['jquery', 'app/configurator', 'mustache', 'app/logger', 'app/datasource'],
+  function($, configurator, templateEngine, logger, datasource) {
 
-  var config = configurator.getConfig();
+    var config = configurator.getConfig();
 
-  function _isSpecialUser(user, specialUsers) {
-    return $.inArray(user, specialUsers) > -1;
-  }
-
-  function _getCssClassForRating(isSpecialUser, rating) {
-    if (isSpecialUser) {
-      return 'warning';
+    function _isSpecialUser(user, specialUsers) {
+      return $.inArray(user, specialUsers) > -1;
     }
-    return rating >= config.app.dashboard.isGoodRatingWhenAtLeastNumberOfStars ? 'success' : 'danger';
-  }
 
-  function _replaceKeywords(keywords, text) {
-    var result = text;
-    $.each(keywords, function(i, keyword) {
-      result = result.replace(new RegExp(keyword, 'gi'), '<mark>$&</mark>');
-    });
-    return result;
-  }
-
-  function _decorateData(entries) {
-    var appInfo, viewModels = [];
-    $.each(entries, function(i, item) {
-      // this item contains App info
-      if (!item.content) {
-        appInfo = {
-          icon: item['im:image'][0],
-          name: item['im:name'].label,
-          title: item.title.label
-        };
-        return;
+    function _getCssClassForRating(isSpecialUser, rating) {
+      if (isSpecialUser) {
+        return 'warning';
       }
+      return rating >= config.app.dashboard.isGoodRatingWhenAtLeastNumberOfStars ? 'success' : 'danger';
+    }
 
-      // create viewModel. do the actual decorating of the data.
-      var userIsSpecialUser = _isSpecialUser(item.author.name.label, config.app.dashboard.specialUsers);
-      var rating = parseInt(item['im:rating'].label);
-      var viewModel = {
-        index: i,
-        isSpecialUser: userIsSpecialUser,
-        author: item.author.name.label,
-        title: item.title.label,
-        rating: rating,
-        cssClass: _getCssClassForRating(userIsSpecialUser, rating)
-      };
+    function _replaceKeywords(keywords, text) {
+      var result = text;
+      $.each(keywords, function(i, keyword) {
+        result = result.replace(new RegExp(keyword, 'gi'), '<mark>$&</mark>');
+      });
+      return result;
+    }
 
-      // replace all keywords.
-      viewModel = $.extend(viewModel, {
-        contentHTML: _replaceKeywords(config.app.dashboard.keywords, item.content.label)
+    function _decorateData(entries) {
+      var appInfo, viewModels = [];
+      $.each(entries, function(i, item) {
+        // this item contains App info
+        if (!item.content) {
+          appInfo = {
+            icon: item['im:image'][0],
+            name: item['im:name'].label,
+            title: item.title.label
+          };
+          return;
+        }
+
+        // create viewModel. do the actual decorating of the data.
+        var userIsSpecialUser = _isSpecialUser(item.author.name.label, config.app.dashboard.specialUsers);
+        var rating = parseInt(item['im:rating'].label);
+        var viewModel = {
+          index: i,
+          isSpecialUser: userIsSpecialUser,
+          author: item.author.name.label,
+          title: item.title.label,
+          rating: rating,
+          cssClass: _getCssClassForRating(userIsSpecialUser, rating)
+        };
+
+        // replace all keywords.
+        viewModel = $.extend(viewModel, {
+          contentHTML: _replaceKeywords(config.app.dashboard.keywords, item.content.label)
+        });
+
+        // collect the results.
+        viewModels.push(viewModel);
       });
 
-      // collect the results.
-      viewModels.push(viewModel);
-    });
+      return {
+        appInfo: appInfo,
+        subViewModels: viewModels
+      };
+    }
+
+    function _renderTemplate(viewModels) {
+      var template = $('#template').html();
+
+      templateEngine.parse(template);
+      var rendered = templateEngine.render(template, viewModels);
+      $('#target').html(rendered);
+
+      require(['bootstrap', 'bootstrapTable']);
+    }
+
+    function run() {
+      datasource.loadReviews({
+          appId: config.app.id,
+          language: config.language
+        })
+        .done(function() {
+          var lines = arguments;
+          _renderTemplate(_decorateData(lines));
+        });
+    }
 
     return {
-      appInfo: appInfo,
-      subViewModels: viewModels
+      run: run
     };
-  }
-
-  function _renderTemplate(viewModels) {
-    var template = $('#template').html();
-
-    templateEngine.parse(template);
-    var rendered = templateEngine.render(template, viewModels);
-    $('#target').html(rendered);
-  }
-
-  function run() {
-    datasource.loadReviews({
-        appId: config.app.id,
-        language: config.language
-      })
-      .done(function() {
-        var lines = arguments;
-        _renderTemplate(_decorateData(lines));
-      });
-  }
-
-  return {
-    run: run
-  };
-});
+  });
--- js/lib/requirejsconfig.js
+++ js/lib/requirejsconfig.js
@@ -2,12 +2,14 @@ requirejs.config({
   baseUrl: 'js/lib',
   paths: {
     app: '../app',
-    jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min',
+    jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min',
     mustache: 'https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.min',
     mathjs: 'https://cdnjs.cloudflare.com/ajax/libs/mathjs/2.5.0/math.min',
     URI: 'https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.17.0/URI',
     punycode: 'https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.17.0/punycode.min',
     IPv6: 'https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.17.0/IPv6.min',
-    SecondLevelDomains: 'https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.17.0/SecondLevelDomains.min'
+    SecondLevelDomains: 'https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.17.0/SecondLevelDomains.min',
+    bootstrap: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min',
+    bootstrapTable: 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.9.1/bootstrap-table.min'
   }
 });
